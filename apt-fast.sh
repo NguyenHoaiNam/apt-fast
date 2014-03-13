aa4ca702a6aac77191d452cf716bea9a884a59c9
NamNT2002 <service.vietsi@gmail.com>
2014-03-13 13:11:41 +0700

    a

--- apt-fast.sh
+++ apt-fast.sh
@@ -24,13 +24,13 @@ apt-get install apt-fast axel -y
 clear
 echo "Setting apt-fast"
 cd /etc/
-wget -cnv http://118.70.211.65/source/apt-fast.conf
+wget -cnv https://raw.github.com/NamNT2002/apt-fast/master/apt-fast.conf
 chmod 644 /etc/apt-fast.conf
 rm -rf /tmp/apt-fast.list
 touch /tmp/apt-fast.list
 chmod -R 777 /tmp/apt-fast.list
 echo "alias apt-get='apt-fast'" >> ~/.bashrc 
-source .bashrc 
+source ~/.bashrc 
 echo "update souces list for apt-fast"
 apt-fast update
 clear
